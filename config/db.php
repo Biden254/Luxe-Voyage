0e9011ec7c649a202c05c4bd7604a91b6a02f49d
Loren Deklerk <loren.deklerk@solutioncommunity.org>
2026-06-08 23:48:26 +0300

    Prepare project for deployment

--- config/db.php
+++ config/db.php
@@ -1,5 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "luxe_voyage", 3307);
+$host = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_DATABASE');
+$conn = mysqli_connect($host, $username, $password, $dbname, 3307);
 
 if (!$conn) {
     die("Database connection failed");
