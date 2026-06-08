3f98bfd688136282b48fc9a131a8658b954bd152
Loren Deklerk <loren.deklerk@solutioncommunity.org>
2026-06-09 00:45:34 +0300

    Removed custom db ports

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ $host = getenv('DB_HOST');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_DATABASE');
-$conn = mysqli_connect($host, $username, $password, $dbname, 3307);
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
 if (!$conn) {
     die("Database connection failed");
